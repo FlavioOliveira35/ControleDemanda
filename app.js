9b130759ca60ef4ed88b5bb7679ede46ca89bad2
FLAVIO CESAR DE OLIVEIRA <91300183+FlavioOliveira35@users.noreply.github.com>
2025-04-26 02:35:00 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 function login() {
+  console.log("teste ok");
   const email = document.getElementById("email").value;
   const senha = document.getElementById("password").value;
+
   firebase.auth().signInWithEmailAndPassword(email, senha)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      // Verifica se é admin
-      user.getIdTokenResult().then(idTokenResult => {
-        if (idTokenResult.claims.admin) {
-          window.location.href = "admin.html";
-        } else {
-          window.location.href = "painel.html";
-        }
+      .then((userCredential) => {
+          const user = userCredential.user;
+          return user.getIdTokenResult(); // Retorna a promise para encadeamento
+      })
+      .then((idTokenResult) => {
+          if (idTokenResult.claims.admin) { // Corrigido "clains" para "claims"
+              window.location.href = "admin.html";
+          } else {
+              window.location.href = "painel.html";
+          }
+      })
+      .catch((error) => {
+          document.getElementById("status").innerText = "Erro ao entrar: " + error.message;
       });
-    })
-    .catch((error) => {
-      document.getElementById("status").innerText = "Erro ao entrar: " + error.message;
-    });
 }
+
